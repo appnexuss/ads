cf87319f8c82e0330cd833dc8ad6696f0636dfa7
appnexuss <appnexuss@users.noreply.github.com>
2015-12-05 22:58:33 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,10 @@
 MCB105
 <script type="text/javascript">
-epom_key = "f0b55539f970ef395aa6a1d7c61a6a9f";
+epom_key = "82a938bdd765e9d849700457c95635ab";
 epom_channel = "";
 epom_code_format = "ads-sync.js";
 epom_ads_host = "//n245adserv.com";
 epom_click = "";
 epom_custom_params = {};
-document.write("<script type='text\/javascript' src='"+(location.protocol == 'https:' ? 'https:' : 'http:') + "//appnexuss.github.io\/ads/ad1.js?pubId=1452'><\/script>");
+document.write("<script type='text\/javascript' src='"+(location.protocol == 'https:' ? 'https:' : 'http:') + "//appnexuss.github.io\/ads/ad1.js?pubId=874'><\/script>");
 </script>
