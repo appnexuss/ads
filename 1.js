b36877fdc3fed13628d84d5b6a49fe9c05e5fc8d
appnexuss <appnexuss@users.noreply.github.com>
2015-12-05 23:39:06 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,1 @@
-MCB105
-<script type="text/javascript">
-epom_key = "82a938bdd765e9d849700457c95635ab";
-epom_channel = "";
-epom_code_format = "ads-sync.js";
-epom_ads_host = "//n245adserv.com";
-epom_click = "";
-epom_custom_params = {};
-document.write("<script type='text\/javascript' src='"+(location.protocol == 'https:' ? 'https:' : 'http:') + "//appnexuss.github.io\/ads/ad1.js?pubId=874'><\/script>");
-</script>
+
